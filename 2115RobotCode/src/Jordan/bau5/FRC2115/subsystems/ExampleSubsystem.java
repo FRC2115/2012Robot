f797e6c6b1c743e9562beda921c6368478448e47
Jordan Giacone <jmgiacone@comcast.net>
2012-01-23 22:05:43 -0600

    Home computer testing
    added a stupid comment in the subsystem.java file

--- 2115RobotCode/src/Jordan/bau5/FRC2115/subsystems/ExampleSubsystem.java
+++ 2115RobotCode/src/Jordan/bau5/FRC2115/subsystems/ExampleSubsystem.java
@@ -13,6 +13,7 @@ public class ExampleSubsystem extends Subsystem {
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+        //System.out.println("TESTING GIT FROM MY HOME COMPUTER");
     }
 }
 

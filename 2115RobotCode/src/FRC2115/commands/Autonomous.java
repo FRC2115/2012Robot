741b3c498a7381b29dd0dec8245a4929856b6087
Peter Kowalczyk <slixpk@gmail.com>
2012-03-19 17:55:46 -0500

    Hopefully working autonomous code

--- 2115RobotCode/src/FRC2115/commands/Autonomous.java
+++ 2115RobotCode/src/FRC2115/commands/Autonomous.java
@@ -1,13 +1,25 @@
 package FRC2115.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 public class Autonomous extends CommandGroup
 {
+    private final double TIME_TO_REV = 3.0;
+    private final double TIME_WHILE_SHOOTING = 4.0;
+    
     public Autonomous()
     {
+        //Align and rev up shooter
         addSequential(new AutoAlign());
-        addSequential(new AutoShooterSpeed());
-        addSequential(new AutoShooterSpeed());
+        addSequential(new AutoShooterSpeed(), TIME_TO_REV);
+        
+        //Run shooter while deploying plunger
+        addParallel(new AutoShooterSpeed(), TIME_WHILE_SHOOTING);
+        addSequential(new DeployPlunger(0.5));
+        addSequential(new WaitCommand(0.5)); //Wait for plunger to come back down
+        addSequential(new DeployPlunger(0.5));
+        
+        //That's all folks!
     }
 }

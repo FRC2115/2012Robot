78775dc23d6a45faacc54d2da091c78daeb73912
Peter Kowalczyk <slixpk@gmail.com>
2012-03-19 17:24:57 -0500

    Reformat Shoot command

--- 2115RobotCode/src/FRC2115/commands/Shoot.java
+++ 2115RobotCode/src/FRC2115/commands/Shoot.java
@@ -1,42 +1,32 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Owner
- */
 package FRC2115.commands;
 
-public class Shoot extends CommandBase {
+public class Shoot extends CommandBase
+{
     
-    public Shoot() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+    public Shoot()
+    {
         requires(shooter);
     }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
+    protected void initialize()
+    {
     }
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
+    protected void execute()
+    {
         shooter.actualShoot();
     }
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
+    protected boolean isFinished()
+    {
         return false;
     }
 
-    // Called once after isFinished returns true
-    protected void end() {
+    protected void end()
+    {
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
+    protected void interrupted()
+    {
     }
 }

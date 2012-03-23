cf2e9c775091e5afe20735125cde884454ed9cc3
Jordan Giacone <Jmgiacone@comcast.net>
2012-03-23 15:16:13 -0500

    Changed the Autonomous code to shoot 3 times with a 1 second wait in between. I also commented out calls to random methods.

--- 2115RobotCode/src/FRC2115/commands/Autonomous.java
+++ 2115RobotCode/src/FRC2115/commands/Autonomous.java
@@ -12,17 +12,21 @@ public class Autonomous extends CommandGroup
     public Autonomous()
     {
         //Wait for some pressure buildup
-        addSequential(new WaitCommand(TIME_INITIAL_WAIT));
+        //addSequential(new WaitCommand(TIME_INITIAL_WAIT));
         
         //Align and rev up shooter
         //addSequential(new AutoAlign());
-        addSequential(new AutoShooterSpeed(), TIME_TO_REV);
+        //addSequential(new AutoShooterSpeed(), TIME_TO_REV);
         
         //Run shooter while deploying plunger
         addParallel(new AutoShooterSpeed(), TIME_WHILE_SHOOTING);
-        addSequential(new DeployPlunger(1.0));
-        addSequential(new WaitCommand(1.0)); //Wait for plunger to come back down
-        addSequential(new DeployPlunger(1.0));
+        
+        for(int i = 0; i <  3; i++)
+        {
+            addSequential(new DeployPlunger(1.0));
+            //Wait for plunger to come back down
+            addSequential(new WaitCommand(1.0)); 
+        }
         
         //That's all folks!
     }

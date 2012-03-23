f4c50be61fb4490c0ed342e2fe58e70ad0e53ff3
Peter Kowalczyk <slixpk@gmail.com>
2012-03-23 12:18:19 -0500

    Add buffer time in autonomous for pressure buildup

--- 2115RobotCode/src/FRC2115/commands/Autonomous.java
+++ 2115RobotCode/src/FRC2115/commands/Autonomous.java
@@ -5,11 +5,15 @@ import edu.wpi.first.wpilibj.command.WaitCommand;
 
 public class Autonomous extends CommandGroup
 {
+    private final double TIME_INITIAL_WAIT = 4.0;
     private final double TIME_TO_REV = 3.0;
     private final double TIME_WHILE_SHOOTING = 4.0;
     
     public Autonomous()
     {
+        //Wait for some pressure buildup
+        addSequential(new WaitCommand(TIME_INITIAL_WAIT));
+        
         //Align and rev up shooter
         //addSequential(new AutoAlign());
         addSequential(new AutoShooterSpeed(), TIME_TO_REV);
